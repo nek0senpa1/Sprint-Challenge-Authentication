7755b0fdcf1f5bf25045972e02acc7e1f96c9cff
amberm <nekosenpai@gmail.com>
2019-03-08 09:47:53 -0700

    yarned, and harmed, Captain!

--- config/routes.js
+++ config/routes.js
@@ -1,4 +1,8 @@
 const axios = require('axios');
+const bcrypt = require ('bcryptjs');
+const jwt = require ('jsonwebtoken');
+
+const duhbee = require('../database/dbConfig')
 
 const { authenticate } = require('../auth/authenticate');
 
@@ -8,8 +12,43 @@ module.exports = server => {
   server.get('/api/jokes', authenticate, getJokes);
 };
 
+
+///////
+  // FUNKY TIME
+////////
+async function addstuff (widget) {
+  let [id] = await duhbee('users').insert(widget)
+  return findThing(id)
+}
+
+function findThing(thing) {
+  return duhbee('users').where({id:thing}).first();
+}
+////
+///////
+
 function register(req, res) {
   // implement user registration
+  const newb = req.body;
+  console.log(newb.password);
+  const hashtag = bcrypt.hashSync(newb.password, 12);
+  newb.password = hashtag;
+
+  addstuff(newb)
+  .then(stuff => {
+    if(stuff){
+
+    res.status(200).json({message:"Newb assigned:", stuff})
+    } else {
+      res.send('Something went wrong...')
+    }
+
+  })
+  .catch(errorz => {
+    res.status(500).json({message: "username already registered..."});
+  })
+
+
 }
 
 function login(req, res) {
--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,9 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: { filename: './database/auth.db3' }, // change this if you want a different name for the database
+
+    // you say that, instructions.... but I see I already have that file made... you're trying to get me aren't you...
+    
     useNullAsDefault: true, // used to avoid warning on console
     migrations: {
       directory: './database/migrations',
